04ec61b088c9690e5c110888fee0e7a9ba34d528
itsumohelp <130024405+itsumohelp@users.noreply.github.com>
2024-10-06 23:19:40 +0900

    Create takashi.cpp

---
+++ 2024/10/takashi.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    string S;
+    cin >> S;
+
+    if(S.substr(S.size() - 3) == "san") {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
